f952acd49994f9811f628a1993172ad571e58aa3
soapMan <107b21867@mailst.cjcu.edu.tw>
2020-11-03 15:39:08 +0800

    2020.11.03

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
-define('_SHOP_NAME', 'Tad點心鋪');
+define('_SHOP_NAME', 'steam蒸汽游戏店');
 define('_DB_HOST', 'localhost');
 define('_DB_ID', 'root');
 define('_DB_PW', '12345');
-define('_DB_NAME', 'mini_shop');
+define('_DB_NAME', 'main');
 define('_UPLOAD_PATH', 'C:/UniServerZ/www/mini_shop/uploads/');
-define('_UPLOAD_URL', 'http://localhost/mini_shop/uploads/');
+define('_UPLOAD_URL', 'http://localhost:8700/mini_shop/uploads/');
 
-$admin_array = ['tad'];
+$admin_array = ['107b21867'];
 // error_reporting(0);
